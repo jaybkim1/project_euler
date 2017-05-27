77271b56f01e441ea13a3da0292908f1854bfa64
NotoKing <jaybkim1@gmail.com>
2017-05-27 10:09:52 +0900

    피보나치수열

---
+++ Project_Euler/src/problems/피보나치수열.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class 피보나치수열 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("피보나치수열을 몇개나 출력하시겠습니까? ");
+		int input = sc.nextInt(); 
+		
+		for(int i=1; i<=input; i++){
+			System.out.print(fibo(i)+" ");
+		}
+	}
+	
+	// 피보나치수열 구하는 메소드
+	public static int fibo(int n){
+		if (n<=1) {
+			return n;
+		}else{
+			return fibo(n-2) + fibo(n-1);
+		}
+		
+	}
+	
+}
